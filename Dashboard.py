5695c2329634cc0d16847ada0f49dcbd9b3d0862
Poly104 <128769582+Poly104@users.noreply.github.com>
2023-08-31 16:44:43 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -73,8 +73,8 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select-year', component_property='value'),
-     Input(component_id='dropdown-statistics', component_property='value')])
+    [Input(component_id='dropdown-statistics', component_property='value'),
+     Input(component_id='select-year', component_property='value')])
 def update_output_container(selected_statistics, input_year):
     if selected_statistics == 'Recession Period Statistics':
         # Filter the data for recession periods
@@ -104,8 +104,8 @@ def update_output_container(selected_statistics, input_year):
                                            title="Total expenditure share by vehicle type during recession"))
 
         # Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
-        df = recession_data.groupby(["Vehicle_Type", "Unemployment_Rate"])["Automobile_Sales"].mean().reset_index()
-        R_chart4 = dcc.Graph(figure=px.bar(df, x="Vehicle_Type", y="Automobile_Sales", color="Unemployment_Rate",
+        df = recession_data.groupby(["Vehicle_Type", "unemployment_rate"])["Automobile_Sales"].mean().reset_index()
+        R_chart4 = dcc.Graph(figure=px.bar(df, x="Vehicle_Type", y="Automobile_Sales", color="unemployment_rate",
                                            title="Effect of unemployment rate on vehicle type and sales"))
 
         return [
